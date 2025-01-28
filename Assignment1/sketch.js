707fb0fffd5fd4d7b6e58c8f0b881ee5ce31c626
Christian Fullerton <116850211+cfull03@users.noreply.github.com>
2025-01-28 13:18:56 -0600

    Add files via upload

---
+++ Assignment1/sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(800, 800);
+  noLoop();
+}
+
+function draw() {
+  background(255);
+  drawPattern1();
+  drawPattern2();
+  drawPattern3();
+}
+
+function drawPattern1() {
+  noStroke();
+  fill(255, 0, 0); 
+  ellipse(200, 200, 150, 150);
+  fill(0, 0, 255);
+  ellipse(300, 200, 100, 100);
+}
+
+function drawPattern2() {
+  noFill();
+  stroke(0); 
+  strokeWeight(2);
+
+  for (let x = 50; x < 750; x += 100) {
+    for (let y = 50; y < 750; y += 100) {
+      rect(x, y, 80, 80);
+    }
+  }
+}
+
+function drawPattern3() {
+  noFill();
+  stroke(0);
+  strokeWeight(3);
+
+  // Draw lines
+  for (let i = 50; i <= 750; i += 50) {
+    line(i, 50, i, 750);
+  }
+
+  // Draw arcs
+  for (let i = 100; i <= 700; i += 100) {
+    arc(i, 400, 100, 100, 0, PI);
+  }
+}
