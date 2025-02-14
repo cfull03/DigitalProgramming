9304e02fddb106b00c6a262c2eb4135fec8a9dc1
Christian Fullerton <116850211+cfull03@users.noreply.github.com>
2025-02-14 15:59:12 -0600

    Update sketch.js

--- Assignment1/image1/sketch.js
+++ Assignment1/image1/sketch.js
@@ -1,14 +1,12 @@
 function setup() {
   createCanvas(400, 400);
-  background(50, 205, 50);
   noStroke();
-  
+}
+
+function draw() {
+  background(50, 205, 50);
+
   fill(255);
   rect(100, 150, 100, 101);
   ellipse(250, 200, 100, 100);
-}
-
-
-function draw() {
-  background(220);
 }
