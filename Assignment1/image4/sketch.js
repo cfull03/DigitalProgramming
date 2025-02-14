cf5c16c95e1f4babd39db48c5d564878cc0fefe8
Christian Fullerton <116850211+cfull03@users.noreply.github.com>
2025-02-14 16:04:27 -0600

    Update sketch.js

--- Assignment1/image4/sketch.js
+++ Assignment1/image4/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   createCanvas(400, 400);
-  background(0, 0, 255);
   noStroke();
+}
+
+function draw() {
+  background(0, 0, 255);
 
   fill(0, 128, 0);
   ellipse(200, 200, 201, 200);
@@ -9,15 +12,12 @@ function setup() {
   fill(255, 0, 0);
   beginShape();
   for (let i = 0; i < 10; i++) {
-      let angle = PI / 5 * i;
-      let r = i % 2 === 0 ? 50 : 100;
-      let x = 200 + cos(angle) * r;
-      let y = 200 + sin(angle) * r;
-      vertex(x, y);
+    let angle = PI / 5 * i;
+    let r = i % 2 === 0 ? 50 : 100;
+    let x = 200 + cos(angle) * r;
+    let y = 200 + sin(angle) * r;
+    vertex(x, y);
   }
   endShape(CLOSE);
 }
 
-function draw() {
-  background(220);
-}
